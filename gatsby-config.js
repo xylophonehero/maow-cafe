f1a8b771c968a72006594df2ceae74373256a2f6
nick <nick97000@gmail.com>
2021-01-25 17:41:28 +0000

    second commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Maow Cafe`,
+    description: `Where all the Maows come to drink coffee and pfit cake.`,
+    author: `@xylophonehero`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
